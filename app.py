9b1f6fd92332b2c4656653fa6f7b17f092e6fb25
nzovia <nickiemaundu19@gmail.com>
2023-04-14 19:37:18 +0300

    taking input from the user

---
+++ app.py
@@ -0,0 +1,16 @@
+# name = 'John Smith'
+# print(name)
+# age = '20 years'
+# print(age)
+# is_new = True
+# #input from user
+
+# persons_name = input('what is your name ')
+# color = input('what is your favourite color? ')
+# print(persons_name +  ' likes '  + color)
+
+#to calculate the age
+year_of_birth = input('Year of Birth:')
+# int() used to convert string year_of_ birth to integer
+age = 2023 - int(year_of_birth)
+print(age)
