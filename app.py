b56aa845d27513c2f2777cd8dbd1cd1736242c52
fidelma <fedymueni18@gmail>
2023-04-15 18:00:31 +0300

    methods of string concatenation

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
-# name = 'John Smith'
-# print(name)
-# age = '20 years'
-# print(age)
-# is_new = True
-# #input from user
+# Python is fun i wanna learn everything
+name = 'John Smith'
+print(name)
+age = '20 years'
+print(age)
+is_new = True
 
-# persons_name = input('what is your name ')
-# color = input('what is your favourite color? ')
-# print(persons_name +  ' likes '  + color)
+# #input from user
+persons_name = input('what is your name ')
+color = input('what is your favourite color? ')
+print(persons_name +  ' likes '  + color)
 
 #to calculate the age
 year_of_birth = input('Year of Birth:')
 # int() used to convert string year_of_ birth to integer
 age = 2023 - int(year_of_birth)
 print(age)
+
+#string concatenation in python
+#first method is to use + sign and the str() function to convert integers into strings
+print('There are ' + str(24) + ' hours in day ')
