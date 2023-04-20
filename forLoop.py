23c9b38bcd2f16d5e2094da804dfec383c7e4877
fidelma <fedymueni18@gmail>
2023-04-20 16:32:19 +0300

    more on loops

--- forLoop.py
+++ forLoop.py
@@ -3,3 +3,15 @@ sum = 0
 for total in prices:
     sum += total
 print(sum)
+#nested for loop
+for x in range(4):
+    for y in range(3):
+        print(f'({x}, {y})')
+
+#challenge
+numbers = [2,2,2,2,5]
+for i in numbers:
+    output = ''
+    for count in range(i):
+        output += 'x'
+    print(output)
